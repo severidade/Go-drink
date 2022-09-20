874c6fae939cd7e6fe3533ecf1d34d6ae36c1a56
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-20 16:53:17 -0300

    feat: Migrate products

---
+++ back-end/src/database/migrations/20220920190952-create-sales-products.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = {
+    /**
+   * @param {import('sequelize').queryInterface} queryInterface 
+   * @param {import('sequelize').DataTypes} Sequelize 
+   */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('salesProducts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      sale_id: {
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('salesProducts');
+  }
+};
