82a74060800535a2a90194beeab3dbb32213956d
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-20 16:52:39 -0300

    feat: Migration Users

---
+++ back-end/src/database/migrations/20220920185236-create-users.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+    /**
+   * @param {import('sequelize').queryInterface} queryInterface 
+   * @param {import('sequelize').DataTypes} Sequelize 
+   */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
