9759ad12d79dee3c78906b64edcc67e4ec146cdc
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-20 16:59:04 -0300

    feat: Model products

---
+++ back-end/src/database/models/products.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+    /**
+   * @param {import('sequelize').queryInterface} queryInterface 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+module.exports = (sequelize, DataTypes) => {
+  class products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      products.hasMany(models.salesProducts,{
+        foreignKey: 'product_id',
+        as: 'products'
+      })
+    }
+  }
+  products.init({
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    urlImage: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'products',
+    timestamps: false,
+    underscored: true
+  });
+  return products;
+};
---
+++ back-end/src/database/models/sales.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class sales extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      sales.hasMany(models.salesProducts,{
+        foreignKey: 'product_id',
+        as: 'sales'
+      })
+
+      sales.belongsTo(models.users,{
+        foreignKey: 'user'
+      })
+    }
+
+  }
+  sales.init({
+    userId: DataTypes.INTEGER,
+    sellerId: DataTypes.INTEGER,
+    totalPrice: DataTypes.DECIMAL,
+    deliveryAddress: DataTypes.STRING,
+    deliveryNumber: DataTypes.STRING,
+    saleDate: DataTypes.DATE,
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'sales',
+    updatedAt: false,
+    underscored:true
+  });
+  return sales;
+};
