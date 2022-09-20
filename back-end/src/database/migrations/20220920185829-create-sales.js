7728b7db0f84c7effd954b7cb243f940d10d54c8
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-20 16:53:07 -0300

    feat: Migrate sales

---
+++ back-end/src/database/migrations/20220920185829-create-sales.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').queryInterface} queryInterface 
+   * @param {import('sequelize').DataTypes} Sequelize 
+   */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        field: 'user_id'
+      },
+      sellerId: {
+        type: Sequelize.INTEGER,
+        field: 'seller_id'
+      },
+      totalPrice: {
+        type: Sequelize.DECIMAL(9,2),
+        field: 'total_price'
+      },
+      deliveryAddress: {
+        type: Sequelize.STRING(100),
+        field: 'delivery_address'
+      },
+      deliveryNumber: {
+        type: Sequelize.STRING(50),
+        field: 'delivery_number'
+      },
+      status: {
+        type: Sequelize.STRING(50)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'sale_date'
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('sales');
+  }
+};
