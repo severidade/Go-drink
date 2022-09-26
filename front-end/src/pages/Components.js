18a47291c8e0373ee020791b8eeefd0c0b55432f
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-26 15:46:51 -0300

    fix: Corrige importação da pagina components

--- front-end/src/pages/Components.js
+++ front-end/src/pages/Components.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import CardProduct from '../components/CardProduct';
-import CheckoutElement from '../components/CheckoutElement';
+import CardProduct from '../components/CardProduct/CardProduct';
+import CheckoutElement from '../components/CheckoutElement/CheckoutElement';
 
 import antartica from '../images/mockup/no_bg/antarctica_pilsen_300ml.png';
 
@@ -8,7 +8,7 @@ function Components() {
   return (
     <>
       {/* <CardProduct productName="Bolacha" price="100,00" url="https://severidade.github.io/img/avatar.png" /> */}
-      <CardProduct productName="Bolacha" price="100,00" url={ antartica } />
+      <CardProduct productName="Bolacha" price="100,00" url={ antartica } id="d" />
       <CheckoutElement productName="Bolacha" url={ antartica } />
     </>
   );
