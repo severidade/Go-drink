0490452384d563828abdeb789e9112079de30248
severo <oliveira.severo@gmail.com>
2022-09-26 15:08:51 -0300

    feat: :art: Checkout componente Inicio

--- front-end/src/pages/Components.js
+++ front-end/src/pages/Components.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import CardProduct from '../components/CardProduct';
+import CheckoutElement from '../components/CheckoutElement';
+
+import antartica from '../images/mockup/no_bg/antarctica_pilsen_300ml.png';
 
 function Components() {
   return (
-    <CardProduct productName="Bolacha" price="100,00" url="https://severidade.github.io/img/avatar.png" />
+    <>
+      {/* <CardProduct productName="Bolacha" price="100,00" url="https://severidade.github.io/img/avatar.png" /> */}
+      <CardProduct productName="Bolacha" price="100,00" url={ antartica } />
+      <CheckoutElement productName="Bolacha" url={ antartica } />
+    </>
   );
 }
 
