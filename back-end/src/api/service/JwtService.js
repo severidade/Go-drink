cc9f94ad9cbeec4df94196ed8628b31cfa43a33a
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-27 17:12:20 -0300

    fix: Recoloca a quebra de linha no secrete do jwt

--- back-end/src/api/service/JwtService.js
+++ back-end/src/api/service/JwtService.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
-const secret = fs.readFileSync('jwt.evaluation.key', { encoding: 'utf-8' }).trim();
+const secret = fs.readFileSync('jwt.evaluation.key', { encoding: 'utf-8' });
 
 const jwtService = {
   createToken: (data) => jwt.sign({ data }, secret),
