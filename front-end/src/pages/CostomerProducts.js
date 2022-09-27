e3af83b89f7e63ed9bc6cfc48a643790e7ae949d
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-27 11:37:15 -0300

    feat: Preenche a pagina products com os produtos

---
+++ front-end/src/pages/CostomerProducts.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import CartButton from '../components/CartButton/CartButton';
+import productsRequest from '../services/requests/productsRequest';
+import CardProduct from '../components/CardProduct/CardProduct';
+
+function CustomerProducts() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    productsRequest.getAll()
+      .then(({ body }) => setProducts(body));
+  }, []);
+
+  return (
+    <div className="container_page">
+      <NavBar user="Cicrano da silva" selected="products" />
+      <h1>PRODUTOS PAGE</h1>
+      <div className="container_products">
+        {products.map((product) => (
+          <CardProduct
+            key={ product.id }
+            id={ product.id }
+            productName={ product.name }
+            url={ product.urlImage }
+            price={ product.price }
+          />
+        ))}
+      </div>
+      <CartButton />
+    </div>
+  );
+}
+
+export default CustomerProducts;
