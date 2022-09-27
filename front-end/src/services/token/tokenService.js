30c0cccc04a84990d8e4623e6b2bf802434ec6f5
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-27 15:24:38 -0300

    feat: Salva o as informações do token no localstorage

--- front-end/src/services/token/tokenService.js
+++ front-end/src/services/token/tokenService.js
@@ -5,8 +5,16 @@ export default {
     const { token } = localStorage;
     return token;
   },
+
   decodeToken(token) {
     const payload = jwtDecode(token);
     return payload.data;
+  },
+
+  saveLocalStorage(token) {
+    const data = this.decodeToken(token);
+    data.token = token;
+    const dataStringify = JSON.stringify(data);
+    localStorage.setItem('user', dataStringify);
   },
 };
