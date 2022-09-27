0728b135582bf98558e0a89cdc1880fd6e6e1f8e
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-27 15:19:13 -0300

    feat: Adiciona a função para decodificar o token

--- front-end/src/services/token/tokenService.js
+++ front-end/src/services/token/tokenService.js
@@ -1,6 +1,12 @@
+import jwtDecode from 'jwt-decode';
+
 export default {
   getToken() {
     const { token } = localStorage;
     return token;
+  },
+  decodeToken(token) {
+    const payload = jwtDecode(token);
+    return payload.data;
   },
 };
