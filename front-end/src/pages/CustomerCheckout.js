698b916fc8adfeda194a77505c09ac98e780160d
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-28 18:10:28 -0300

    feat: Adiciona a pagina CustomerCheckout

---
+++ front-end/src/pages/CustomerCheckout.js
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react';
+import CheckoutElement from '../components/CheckoutElement/CheckoutElement';
+import CustomerCheckoutDetails
+  from '../components/CustomerCheckoutDetails/CustomerCheckoutDetails';
+import NavBar from '../components/NavBar/NavBar';
+import CartContext from '../context/CartContext';
+
+function CustomerCheckout() {
+  const {
+    cartList,
+    cartTotalPrice,
+  } = useContext(CartContext);
+  return (
+    <div>
+      <NavBar selected="products" />
+      <h1>Finalizar Pedidos</h1>
+      <div>
+        {
+          cartList.map((e, ind) => (
+            <CheckoutElement
+              key={ e.id }
+              itemNumber={ ind }
+              productName={ e.productName }
+              quantity={ e.quantity }
+              price={ e.price }
+              url={ e.url }
+              id={ e.id }
+            />
+          ))
+        }
+
+        <p>
+          Total: R$
+          {' '}
+          <span
+            data-testid="customer_checkout__element-order-total-price"
+          >
+            {cartTotalPrice().replace('.', ',')}
+
+          </span>
+        </p>
+      </div>
+      <h1>Detalhes e endereço para entrega</h1>
+      <CustomerCheckoutDetails />
+      <button
+        type="button"
+        data-testid="customer_checkout__button-submit-order"
+        onClick={ () => {} }
+      >
+        Finalizar Pedido
+
+      </button>
+    </div>
+  );
+}
+
+export default CustomerCheckout;
