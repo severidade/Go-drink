834d9025561f84e0db599231cfce8861ae12809f
Leandroswq <leandrobonfim15@hotmail.com>
2022-10-01 11:41:32 -0300

    feat: Componente OrderDetails

---
+++ front-end/src/components/OrderDetails/OrderDetails.js
@@ -0,0 +1,100 @@
+import PropTypes from 'prop-types';
+import ordersRequest from '../../services/requests/ordersRequest';
+import saleStatus from '../../services/status/saleStatus';
+
+function OrderDetails({
+  id,
+  seller,
+  date,
+  status,
+  testidPrefix,
+  isUser,
+}) {
+  function handleCLickDeliveryCheck() {
+    ordersRequest(id, saleStatus.entregue);
+  }
+
+  function handleCLickPreparingCheck() {
+    ordersRequest(id, saleStatus.preparando);
+  }
+
+  function handleCLickDispatchCheck() {
+    ordersRequest(id, saleStatus.emTransito);
+  }
+  return (
+    <div>
+      <p
+        data-testid={ `${testidPrefix}element-order-details-label-order-id` }
+      >
+        {`Pedido ${id}`}
+
+      </p>
+      {
+        isUser && (
+          <p
+            data-testid={ `${testidPrefix}element-order-details-label-seller-name` }
+          >
+            {seller}
+          </p>
+        )
+      }
+      <p
+        data-testid={ `${testidPrefix}element-order-details-label-order-date` }
+      >
+        {date}
+      </p>
+      {
+        isUser ? (
+          <>
+            <p
+              data-testid={ `${testidPrefix}element-order-details-label-delivery-status` }
+            >
+              {status}
+            </p>
+            <button
+              data-testid={ `${testidPrefix}button-delivery-check` }
+              type="button"
+              onClick={ handleCLickDeliveryCheck }
+            >
+              Marcar como entregue
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              data-testid={ `${testidPrefix}button-preparing-check` }
+              type="button"
+              onClick={ handleCLickPreparingCheck }
+            >
+              Preparar pedido
+            </button>
+            <button
+              type="button"
+              data-testid={ `${testidPrefix}button-dispatch-check` }
+              onClick={ handleCLickDispatchCheck }
+            >
+              Saiu para entrega
+            </button>
+          </>
+        )
+      }
+
+    </div>
+  );
+}
+
+OrderDetails.propTypes = {
+  id: PropTypes.number.isRequired,
+  seller: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  testidPrefix: PropTypes.string.isRequired,
+  isUser: PropTypes.bool,
+};
+
+OrderDetails.defaultProps = {
+  isUser: false,
+
+};
+
+export default OrderDetails;
