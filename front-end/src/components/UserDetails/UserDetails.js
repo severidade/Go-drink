80038a68731450a01f3225e83b927c970a358037
Leandroswq <leandrobonfim15@hotmail.com>
2022-10-23 18:18:47 -0300

    feat<front>: Componente que mostra os detalhes do usuário

---
+++ front-end/src/components/UserDetails/UserDetails.js
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+
+function UserDetails({ userName, email, role, itemNumber, removeItem }) {
+  return (
+    <div>
+      <p
+        data-testid={ `admin_manage__element-user-table-item-number-${itemNumber}` }
+      >
+        {itemNumber}
+
+      </p>
+      <p
+        data-testid={ `admin_manage__element-user-table-name-number-${itemNumber}` }
+      >
+        {userName}
+
+      </p>
+      <p
+        data-testid={ `admin_manage__element-user-table-email-${itemNumber}` }
+      >
+        {email}
+
+      </p>
+      <p
+        data-testid={ `admin_manage__element-user-table-role-${itemNumber}` }
+      >
+        {role}
+
+      </p>
+      <button
+        type="button"
+        onClick={ removeItem }
+        data-testid={ `admin_manage__element-user-table-remove-${itemNumber}` }
+      >
+        Excluir
+
+      </button>
+    </div>
+  );
+}
+
+export default UserDetails;
+
+UserDetails.propTypes = {
+  userName: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+  itemNumber: PropTypes.number.isRequired,
+  removeItem: PropTypes.func.isRequired,
+};
