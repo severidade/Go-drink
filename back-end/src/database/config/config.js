c69b4e3c7827b3c16c81d02cdee6c9252d695dcc
Leandroswq <leandrobonfim15@hotmail.com>
2022-12-03 10:29:19 -0300

    fix: Correção na configuração do db no backend

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
+// O nome da environment é NODE_ENVS para evitar conflitos
+const environment = process.env.NODE_ENVS || "test";
 
 const suffix = {
   prod: "",
@@ -11,10 +12,10 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
+  host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  database:
+    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment]}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
