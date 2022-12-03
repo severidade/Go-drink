246a6c6ef1f4d27af92774a5c8403ec56bc558cf
Leandroswq <leandrobonfim15@hotmail.com>
2022-12-03 10:25:16 -0300

    feat: Exibe no console o erro do middleware de erro

--- back-end/src/api/middlewares/errorMiddleware.js
+++ back-end/src/api/middlewares/errorMiddleware.js
@@ -2,6 +2,7 @@ const errorFunc = (err, _req, res, _next) => {
     if (err.status) return res.status(err.status).json({ message: err.message });
 
     res.status(500).json({ message: err.message });
+    console.log(err);
   };
 
 module.exports = errorFunc;
