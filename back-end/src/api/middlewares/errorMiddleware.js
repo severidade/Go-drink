b5fe625355499484519c70e48e1d43be16ce2568
Leandroswq <leandrobonfim15@hotmail.com>
2022-12-03 10:35:32 -0300

    fix: Aletera a mensagem de erro para o caso de erro 500

--- back-end/src/api/middlewares/errorMiddleware.js
+++ back-end/src/api/middlewares/errorMiddleware.js
@@ -1,7 +1,7 @@
 const errorFunc = (err, _req, res, _next) => {
     if (err.status) return res.status(err.status).json({ message: err.message });
 
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: "Erro interno no servidor" });
     console.log(err);
   };
 
